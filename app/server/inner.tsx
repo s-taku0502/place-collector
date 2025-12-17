29ae719c36c37cb34da6cb7f1a95c770d6ced601
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-12-18 01:25:36 +0900

    ボタンの仕組みの修正

--- app/server/inner.tsx
+++ app/server/inner.tsx
@@ -6,10 +6,9 @@ import { api } from "../../convex/_generated/api";
 export default function Home({
   preloaded,
 }: {
-preloaded: Preloaded<typeof api.places.list>;
+  preloaded: Preloaded<typeof api.places.list>;
 }) {
   const data = usePreloadedQuery(preloaded);
-  const addNumber = useMutation(api.places.add);
   return (
     <>
       <div className="flex flex-col gap-4 bg-slate-100 dark:bg-slate-800 border border-slate-300 dark:border-slate-600 p-6 rounded-xl shadow-md">
@@ -22,14 +21,6 @@ preloaded: Preloaded<typeof api.places.list>;
           </pre>
         </code>
       </div>
-      <button
-        className="bg-slate-700 hover:bg-slate-800 dark:bg-slate-600 dark:hover:bg-slate-500 text-white px-6 py-3 rounded-lg mx-auto cursor-pointer transition-all duration-200 shadow-md hover:shadow-lg font-medium"
-        onClick={() => {
-          void addNumber({ value: Math.floor(Math.random() * 10) });
-        }}
-      >
-        Add a random number
-      </button>
     </>
   );
 }
