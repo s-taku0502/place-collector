5f53f620f3d6d91a91b75ad4ef658f687e10266c
s-taku0502 <takumi.daimonhigh@gmail.com>
2025-12-18 14:24:26 +0900

    gemini code reviewの対応

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-import { useQuery, useMutation } from "convex/react";
-import { redirect, useRouter } from "next/navigation";
-import { api } from "@/convex/_generated/api";
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
-  
-  const places = useQuery(api.places.list, {});
-  const toggleVisited = useMutation(api.places.toggleStatus); // 行った・行ってないを切り替える
-
-  return (
-    redirect("/place")
-  );
+  redirect("/place");
 }
