a38a108f08c372aacf2ec30203ca17af6fa86ca1
s-taku0502 <takumi.daimonhigh@gmail.com>
2026-03-05 13:44:36 +0900

    エラー対処

--- middleware.ts
+++ middleware.ts
@@ -25,5 +25,5 @@ export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
 export const config = {
   // The following matcher runs middleware on all routes
   // except static assets.
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "\/(api|trpc)(.*)"],
 };
